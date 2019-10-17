0e8540f2e4f6dde8185613b341b134f668c843b5
Koiji <55895174+Koiji@users.noreply.github.com>
2019-10-17 14:28:45 +0300

    Revert "Create z59.py"
    This reverts commit 15f6480638d4d6373cf2813be4a52edf049d8dc4.

--- z59.py
+++
@@ -1,9 +0,0 @@
-import math
-x = int(input())
-print("x")
-y= int(input())
-print("y")
-if  x <= 0 and y <= -x and y >= -2 or x >= 0 and y <= x and y >= -2:
-    print ("Tochka lejit na ploskosti")
-else:
-    print ("Tochka ne lejit na ploskosti")
