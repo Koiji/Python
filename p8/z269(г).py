c9f18bbf58b8350e4fe8abd1f014567ffc2104a2
Koiji <55895174+Koiji@users.noreply.github.com>
2019-12-05 12:07:09 +0200

    Init

--- p8/z269(г).py
+++ p8/z269(г).py
@@ -5,8 +5,8 @@ len2 = len(second)
 count = 0
 for i in range(len1):
  if i < len2 and first[i] == second[i]:
- count += 1
- continue
+  count += 1
+  continue
  else:
- break
-return count
+  break
+  return 0
